e27437de3e491ee120fb387b776ba96032535ccc
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2017-02-20 15:54:38 +0100

    Added directory for exercise 1
    Added example files with TODOs.

---
+++ ue01/moneycounter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Money Counter</title>
+</head>
+<body>
+<?php
+// TODO: Get the data from the form in index.html
+
+// TODO: Create a regular expression that matches the required criteria
+
+// TODO: Match amounts of money using the regex and sum up the result
+
+// TODO: Display the resulting amount
+?>
+</body>
+</html>
