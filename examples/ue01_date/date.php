f93c8f58cc24c5ce9e43db75df3f0f91aa2a2de7
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2017-02-22 14:21:11 +0100

    Added footer

--- examples/ue01_date/date.php
+++ examples/ue01_date/date.php
@@ -38,5 +38,11 @@
         </div>
     </section>
 </main>
+<footer class="Site-footer">
+    <div class="Footer Footer--small">
+        <p class="Footer-credits">Created and maintained by <a href="mailto:martin.harrer@fh-hagenberg.at">Martin Harrer</a> and <a href="mailto:wolfgang.hochleitner@fh-hagenberg.at">Wolfgang Hochleitner</a>.</p>
+        <p class="Footer-version"><i class="fa fa-calendar" aria-hidden="true"></i><a href="https://github.com/Digital-Media/phpue">Date Conversion Version 2017</a></p>
+    </div>
+</footer>
 </body>
 </html>
